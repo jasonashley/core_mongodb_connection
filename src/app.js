10085deacd74f8bcb408e5aeaf6b5fdc300f00fd
Jason Ashley <tradingbills@gmail.com>
2018-10-05 19:15:17 +0100

    refactor: separate db from results, use old-school callback

--- src/app.js
+++ src/app.js
@@ -7,20 +7,29 @@ let mongodb = require("mongodb"),
 // let mongoUrl = 'mongodb://localhost:27017';
 let mongoUrl = `${process.env.atlas_base_url}`;
 
-mongodb.MongoClient.connect(
-  mongoUrl,
-  (err, client) => {
-    assert.equal(null, err);
-    console.log("Successfully connected to server");
-    // assign the db to use here, instead of using the URL to set the db
-    const db = client.db("video");
+const getDb = next => {
+  mongodb.MongoClient.connect(
+    mongoUrl,
+    (err, client) => {
+      assert.equal(null, err);
+      console.log("Successfully connected to server");
+      // assign the db to use here, instead of using the URL to set the db
+      const db = client.db("video");
 
+      next(err, db);
+    }
+  );
+};
+
+let showMovieDetails = () =>
+  getDb((err, db) => {
     db.collection("movieDetails")
       .find({})
-      .toArray((err, docs) => {
+      .toArray((_err, docs) => {
         docs.forEach(doc => {
           console.log(doc.title);
         });
       });
-  }
-);
+  });
+
+showMovieDetails();
